36a67089c204bbae1fae83bff531c3b1c171b2f3
unknown <sg-05>
2015-09-30 13:31:05 -0500

    modifique form

--- hoteles/hoteles/forms.py
+++ hoteles/hoteles/forms.py
@@ -13,3 +13,9 @@ class HotelForm(forms.ModelForm):
     class Meta:
         model = DatosHotel
         fields = ('nombre','telefono','direccion','colonia','ciudad')
+
+class HotelForm(forms.ModelFormUser):
+
+    class Meta2:
+        model = DatosHotelUser
+        fields = ('nombre','telefono','direccion','colonia','ciudad', user)
